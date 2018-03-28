e17e14fcfdfdfba16a9fa959cfee4b8286d4bc6d
J H <jahalu88@hotmail.com>
2018-03-28 19:15:22 -0400

    added comments/cleaned code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 
+
+// Alphabet array
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
 'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
@@ -7,14 +9,13 @@ var wins = 0;
 var losses = 0;
 var guesses = 10;
 
-
+// Random letter generator
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(computerChoice)
 
+// Function when user presses key
 document.onkeyup = function(event) {
     var userGuess = event.key;
-    
-    
 
     if(userGuess === computerChoice){
         wins++;
@@ -34,12 +35,14 @@ document.onkeyup = function(event) {
     document.getElementById('wins').innerHTML = wins;
     document.getElementById('losses').innerHTML = losses;
     document.getElementById('guesses').innerHTML = guesses;
-
+   
 };
 
+// Game Over Function to Reset
 function setGameOver() {
     computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(computerChoice);
     document.getElementById('previousg').innerHTML =" ";
     guesses=10;
 }
+
